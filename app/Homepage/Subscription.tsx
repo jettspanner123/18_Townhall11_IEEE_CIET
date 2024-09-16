550b3e6161b2c29f00b5b6e5e5891a37a0915d3c
Uddeshya Singh <uddeshyasingh12bsci@gmail.com>
2024-09-16 15:32:12 +0530

    fouth commit

--- app/Homepage/Subscription.tsx
+++ app/Homepage/Subscription.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import {SECONDARY_COLOR} from "@/app/constants";
+import {SECONDARY_COLOR, TEXT_ACCENT} from "@/app/constants";
 
 const Subscription = (): React.JSX.Element => {
     return (
         <section style={{background: SECONDARY_COLOR}} className={`w-screen h-screen flex flex-col justify-center items-center`}>
             <h1 className={`text-[15rem] text-white`}>10% off</h1>
             <h1 className={`text-white text-[5rem]`}>home order and newzletter subscription</h1>
-            <p className={`font-light text-white`}>Hello world my name is uddeshya singh and tushar and utsav is gae copples.</p>
+            <p
+                style={{
+                    color: TEXT_ACCENT
+                }}
+                className={`font-light text-white leading-none text-[1.5rem] `}>Hello world my name is uddeshya singh and tushar and utsav is gae copples.</p>
         </section>
     )
 }
